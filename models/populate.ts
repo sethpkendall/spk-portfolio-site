6169d7d6483fa3ddc91a3cb3ebce53625f0cd5eb
Seth <sethpkendall@gmail.com>
2025-05-26 08:32:53 -0700

    Fix type error in database demo population script

--- models/populate.ts
+++ models/populate.ts
@@ -141,15 +141,12 @@ export async function mpPopulate() {
     type: "Dinner"
   });
   const foodId1 = await mpDB.foods.add({
-    mealId,
     title: "Hamburgers"
   });
   const foodId2 = await mpDB.foods.add({
-    mealId,
     title: "French Fries"
   });
   const foodId3 = await mpDB.foods.add({
-    mealId,
     title: "Garden Salad"
   });
   await mpDB.recipes.bulkAdd([
